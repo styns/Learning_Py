c07f81dc7757b81c41febb5d3ba5070707132913
styns <stynsonline@gmail.com>
2020-10-26 16:28:29 +0100

    type checking test

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,21 +1,12 @@
-x1 = 5.12
-float(x1)
-x2= 9.333
-float(x2)
-#Steps = 12
-#R = Steps-2
-##for i in range (R):
-  #  x3 = x1+x2
-    #x1=x2
-    #x2=x3
- ##   print x2
+x1 = 5.1
+x2= 9
 y1 = 11
-float(y1)
-y2 = 23.3
+y2 = 23
 float(y2)
 print(x2,x1,y1,y2)
 print(y2-y1)
 print(x2-x1)
-Rico= (y2-y1) / (x2-x1)
-float(Rico)
-print(Rico)
+print((y2-y1) / (x2-x1))
+a=type(x1)
+b=type(y1)
+print(a,b)
