a4e65c25254a4c95607e904d2790e84b1b4660ba
styns <stynsonline@gmail.com>
2020-10-26 16:06:18 +0100

    Rico of two points

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,14 +1,21 @@
-x1 = 1
-x2= 1
-Steps = 12
-R = Steps-2
-
-print x1
-print x2
-
-for i in range (R):
-    x3 = x1+x2
-    x1=x2
-    x2=x3
-    print x2
-
+x1 = 5.12
+float(x1)
+x2= 9.333
+float(x2)
+#Steps = 12
+#R = Steps-2
+##for i in range (R):
+  #  x3 = x1+x2
+    #x1=x2
+    #x2=x3
+ ##   print x2
+y1 = 11
+float(y1)
+y2 = 23.3
+float(y2)
+print(x2,x1,y1,y2)
+print(y2-y1)
+print(x2-x1)
+Rico= (y2-y1) / (x2-x1)
+float(Rico)
+print(Rico)
