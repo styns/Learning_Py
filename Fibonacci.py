2123efb5f7491e8ecc33f1666bf2c56d8ae8d5ad
styns <stynsonline@gmail.com>
2020-10-27 09:45:06 +0100

    FizzBuzz

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,12 +1,10 @@
-x1 = 5.1
-x2= 9
-y1 = 11
-y2 = 23
-float(y2)
-print(x2,x1,y1,y2)
-print(y2-y1)
-print(x2-x1)
-print((y2-y1) / (x2-x1))
-a=type(x1)
-b=type(y1)
-print(a,b)
+for i in range(1, 16):
+        if i%3 == 0:
+            print('Fizz')
+        else:
+            if i%5 == 0 and i%3==0:
+                print('FizzBuzz')
+            else:
+                if i%5 == 0:
+                    print('buzz')
+            print(i)
